c0610b52545adfb75e69162e6f58587c546c6ac2
Enzo Tonon Morente <enzotmorente@gmail.com>
2023-06-08 12:04:19 -0300

    modelo_arrumado

--- object_detection/model.py
+++ object_detection/model.py
@@ -35,69 +35,90 @@ architecture_config = [
     (3, 1024, 1, 1),
 ]
 
-class CNNBlock(nn.Modu):
+class CNNBlock(nn.Module):
     def __init__(self, in_channels, out_channels, **kwargs):
         super(CNNBlock, self).__init__()
-        self.conv = nn.Convd2d(in_channels, out_channels, bias=False, **kwargs)                
+        self.conv = nn.Conv2d(in_channels, out_channels, bias=False, **kwargs)                
         self.batchnorm = nn.BatchNorm2d(out_channels)
         self.leakyrelu = nn.LeakyReLU(0.1)
 
-    def foward(self, x):
+    def forward(self, x):
         return self.leakyrelu(self.batchnorm(self.conv(x)))
     
 class Yolov1(nn.Module):
-    def __init__(self, *args, **kwargs) -> None:
-        def __init__(self, in_channels=3, **kwargs):
-            super(Yolov1, self).__init__()
-            self.architecture = architecture_config
-            self.in_channels = in_channels,
-            self.darknet = self.create_conv_layers(self.architecture)
-            self.fcs = self.create_fcs(**kwargs)
+    def __init__(self, in_channels=3, **kwargs):
+        super(Yolov1, self).__init__()
+        self.architecture = architecture_config
+        self.in_channels = in_channels
+        self.darknet = self.create_conv_layers(self.architecture)
+        self.fcs = self.create_fcs(**kwargs)
 
-        def foward(self, x):
-            x = self.darknet(x)
-            return self.fcs(torch.flatten(x, start_dim=1))
+    def forward(self, x):
+        x = self.darknet(x)
+        return self.fcs(torch.flatten(x, start_dim=1))
 
-        def _create_conv_layers(self, architecture):
-            layers = []
-            in_channels = self.in_channels
+    def create_conv_layers(self, architecture):
+        layers = []
+        in_channels = self.in_channels
 
-            for x in architecture:
-                if type(x) == tuple:
+        for x in architecture:
+            if type(x) == tuple:
+                layers += [
+                    CNNBlock(
+                    in_channels, x[1], kernel_size=x[0], stride=x[2], padding=x[3],
+                    )   
+                ]
+
+                in_channels = x[1]
+
+            elif type(x) == str:
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            
+            elif type(x) == list:
+                conv1 = x[0] #filtro 1
+                conv2 = x[1] #filtro 2
+                num_repeats = x[2] #quantas vezes esses filtros vão se repetir
+
+                for _ in range(num_repeats):
                     layers += [
                         CNNBlock(
-                        in_channels, x[1], kernel_size=x[0], stride=x[2], padding=x[3]
-                        )   
+                            in_channels,
+                            conv1[1],
+                            kernel_size=conv1[0],
+                            stride=conv1[2],
+                            padding=conv1[3],
+                        )
                     ]
-                elif type(x) == str:
-                    layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
-                
-                elif type(x) == list:
-                    conv1 = x[0] #filtro 1
-                    conv2 = x[1] #filtro 2
-                    num_repeats = x[2] #quantas vezes esses filtros vão se repetir
 
-                    for _ in range(num_repeats):
-                        layers += [
-                            CNNBlock(
-                                in_channels,
-                                conv1[1],
-                                kernel_size=conv1[0],
-                                stride=conv1[2],
-                                padding=conv1[3]
-                            )
-                        ]
+                    layers += [
+                        CNNBlock(
+                            conv1[1], #in_channel vai ser o otuput da priemira layer convolucional
+                            conv2[1], 
+                            kernel_size=conv2[0],
+                            stride=conv2[2],
+                            padding=conv2[3],
 
-                        layers += [
-                            CNNBlock(
-                                conv1[1], #in_channel vai ser o otuput da priemira layer convolucional
-                                conv2[1], 
-                                kernel_size=conv2[3],
-                                padding=conv2[3]
+                        )
+                    ]
 
-                            )
-                        ]
+                    in_channels = conv2[1]
+                    
+        return nn.Sequential(*layers)
+    
+    def create_fcs(self, split_size, num_boxes, num_classes):
+        S, B, C = split_size, num_boxes, num_classes
 
-                        in_channels = conv2[1]
-                        
-            return nn.Sequential(*layers)
+        return nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(1024 * S * S, 496), # no artigo deveria ser 4096, mas para n consumir muita ram diminui-se para 496
+            nn.Dropout(0.0),
+            nn.LeakyReLU(0.1),
+            nn.Linear(496, S * S * (C + B * 5)) # o final deve ser (S, S, 30), logo C + B * 5 = 30
+        )
+
+def test(S=7, B=2, C=20):
+    model = Yolov1(split_size=S, num_boxes=B, num_classes=C)
+    x = torch.randn((2, 3, 448, 448))
+    print(model(x).shape)
+
+test()
