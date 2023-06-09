cabb1c230ee3a8bddc3ad36f5e5cde7eda3500f6
Enzo Tonon Morente <enzotmorente@gmail.com>
2023-06-09 14:46:46 -0300

    ia_funcionando

---
+++ object_detector/files/prediction.py
@@ -0,0 +1,14 @@
+from ultralytics import YOLO    
+import os
+
+#pegar o diretório do nosso modelo já treinado
+model_path = os.path.join("/home/enzo/Desktop/R2_D2/object_detector", "files", "runs", "detect", "train5", "weights", "last.pt") 
+
+image_path = os.path.join("/home/enzo/Desktop/R2_D2/object_detector", "data", "test", "images")
+
+#carregar o modelo treinado
+model = YOLO(model_path)
+
+for image in os.listdir(image_path):
+    path = os.path.join(image_path, image)
+    model.predict(source=path, show=True, hide_labels=False, line_thickness=2)
