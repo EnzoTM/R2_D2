b153ac5e5b89ade47517cdf9984a39135a67becd
Enzo Tonon Morente <enzotmorente@gmail.com>
2023-07-10 15:12:19 -0300

    bag_of_words

--- bag_of_words/training.py
+++ bag_of_words/training.py
@@ -1,16 +1,16 @@
 import json
 import pickle
 import numpy as np
-
 import random
 
 
 import nltk
 from nltk.stem import WordNetLemmatizer #reduz palavras diferentes para uma só
 
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Activation, Dropout
-from tensorflow.keras.optmizers import SGD
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import SGD
 
 lemmatizer = WordNetLemmatizer() #irá cuidar de lematizar as palavras
 
@@ -45,7 +45,7 @@ palavras = list(set(palavras))
 pickle.dump(palavras, open("palavras.pkl", "wb"))
 pickle.dump(classes, open("classes.pkl", "wb"))
 
-training = []
+inputs = []
 output = [0] * len(classes)
 
 for comando in comandos:
@@ -65,10 +65,10 @@ for comando in comandos:
 
     output[classes.index(comandos[1])] = 1
 
-    training.append([bag, output])
+    inputs.append([bag, output])
 
-random.shuffle(training)
-training = np.array(training)
+random.shuffle(inputs)
+training = np.array(inputs)
 
 training_x = []
 
@@ -80,16 +80,37 @@ for i in range(0, len(training[0])):
 for i in range(0, len(training[1])):
     training_y.append(training[1][i])
 
-model = Sequential()
-model.add(Dense(128, input_shape=(len(training_x[0],), activation="relu"))) 
-model.add(Dropout(0.5))
-model.add(Desne(64, activation="relu"))
-model.add(Dropout(0.5))
-model.add(Desne(len(training_y[0]), activation="softmax"))
+ #cirar o medelo 
+    model = Sequential(
+        [
+            #128 input layers, com o tamanho do numero de palavras, e o a activation funcion como relu
+            Dense(128, activation="relu", input_shape=(len(inputs[0]),),), 
+            #50% de dropout
+            Dropout(0.5),
+            #64 hidden layers, com acitivation function como relu
+            Dense(64, activation="relu"),
+            #50% de dropout
+            Dropout(0.5),
+            #x outptus layers, onde x = numero de tag, activation function como softmax (soma tudo ate 1, mostrando a probabilidade)
+            Dense(len(output[0]), activation="softmax")
+        ]
+    )   
 
-sgd = SGD(Ir=0.01, decay=1e-6, momentum=0.9, nestrov=True)
-model.compile(loss="categorical_crossentropy", optimizer=sgd, metrics=["accuracy"])
+#Gradient descent (with momentum) optimizer. Parte q eu n entendo, mas a internet é boa, eu confio.
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True) 
 
-model.fit(np.array(training_x), np.array(training_y), epochs=200, batch_size=5, verbose=1)
+"""
+loss funciton como categorical_crossentropy
+optimizer como o que acabamos de declrar em cima
+e o objeto é acerto (metrics)
+"""
+model.compile(loss="categorical_crossentropy", optimizer=sgd, metrics=["accuracy"]) #configurar o modelo
 
-model.save()
+"""
+np.array(inputs) como input 
+np.array(output) como output
+trinar 200 vezes
+batch_size=5, ou seja, a cada 5 iterações ele atualizará os pesos
+"""
+
+trained_model = model.fit(np.array(inputs), np.array(output), epochs=200, batch_size=5) #treinar o modelo
