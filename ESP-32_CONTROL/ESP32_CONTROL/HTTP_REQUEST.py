8930bfba317a41203169d044d6fdeecd78552388
Caue Paiva <128509727+caue-paiva@users.noreply.github.com>
2023-07-16 15:52:36 -0300

    Update HTTP_REQUEST.py

--- ESP-32_CONTROL/ESP32_CONTROL/HTTP_REQUEST.py
+++ ESP-32_CONTROL/ESP32_CONTROL/HTTP_REQUEST.py
@@ -1,4 +1,4 @@
-import requests
+import requests   // codigo em python utilizado para mandar HTTP requests e controlar o ESP32
 ip = "" #ip gerado pelo ESP32
 def send_request(string):
     url = f'http://{ip}/{string}'
